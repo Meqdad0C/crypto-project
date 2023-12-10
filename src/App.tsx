30992dfacd9cf266868b3fce6aea316e4b99f079
Meqdad Amr <meqdad_@outlook.com>
2023-12-10 23:52:07 +0200

    main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,58 @@
-import { useState } from 'react'
-import { Button } from './components/ui/button'
+import { Button } from '@/components/ui/button'
+import { Textarea } from './components/ui/textarea'
+import { Label } from './components/ui/label'
+import { ModeToggle } from './components/mode.toggle'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className="bg-red-400">Hello World</h1>
-      <p>Count: {count}</p>
-      <Button onClick={() => setCount(count + 1)}>Click me</Button>
-    </>
+    <div>
+      <div className="absolute right-5 top-5">
+        <ModeToggle />
+      </div>
+      <h1 className="pt-10 text-center text-4xl font-bold text-gray-900 dark:text-gray-100">
+        CSE451 - Project
+      </h1>
+      <div className="container h-full py-6">
+        <div className="grid h-full items-stretch gap-6 md:grid-cols-[1fr_200px]">
+          <div className="md:order-1">
+            <div className="flex flex-col space-y-4">
+              <div className="grid h-full gap-6 lg:grid-cols-2">
+                <div className="flex flex-col space-y-4">
+                  <div className="flex flex-1 flex-col space-y-2">
+                    <Label htmlFor="input">Input</Label>
+                    <Textarea
+                      id="input"
+                      placeholder="We is going to the market."
+                      className="flex-1 lg:min-h-[580px]"
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-2">
+                    <Label htmlFor="instructions">Instructions</Label>
+                    <Textarea
+                      id="instructions"
+                      placeholder="Fix the grammar."
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col space-y-4">
+                  <div className="flex flex-1 flex-col space-y-2">
+                    <Label htmlFor="output">Output</Label>
+                    <Textarea
+                      id="output"
+                      placeholder="We is going to the market."
+                      className="flex-1 lg:min-h-[580px]"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Button>Submit</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
