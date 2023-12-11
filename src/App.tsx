554ab15807a1c7e7388e34c2de71e2a8965d5345
Meqdad Amr <meqdad_@outlook.com>
2023-12-11 06:54:42 +0200

    aes done

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,8 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import AES from 'crypto-js/aes'
 import CryptoJS from 'crypto-js'
 import forge from 'node-forge'
+import { useState } from 'react'
+import clsx from 'clsx'
 
 const ButtonWrapper = ({ children }: { children: React.ReactNode }) => (
   <div className="flex items-center space-x-2">
@@ -32,30 +34,21 @@ const ButtonWrapper = ({ children }: { children: React.ReactNode }) => (
       onClick={() => {
         const encrypted = AES.encrypt(
           'ana ba7abk ya bedonzy, yours Hala',
-          CryptoJS.enc.Hex.parse(
-            '0003333333333333333333333333333333333333333333333333333330',
-          ),
+          CryptoJS.enc.Hex.parse('000102030405060708090a0b0c0d0e0f'),
           {
-            iv: CryptoJS.enc.Hex.parse(
-              '101112131415161718191a1b1c1d1e1f101112131415161718191a1b1c1d1e1f101112131415161718191a1b1c1d1e1f101112131415161718191a1b1c1d1e1f',
-            ),
+            iv: CryptoJS.enc.Hex.parse('101112131415161718191a1b1c1d1e1f'),
             mode: CryptoJS.mode.CFB,
           },
         )
         console.log('[encrypted]', encrypted.toString())
-        console.log('[ciphertext]', encrypted.ciphertext.toString())
         // console.log('[salt]', encrypted.salt.toString())
         console.log('[iv]', encrypted.iv.toString())
         console.log('[key]', encrypted.key.toString())
         const decrypted = AES.decrypt(
           encrypted.toString(),
-          CryptoJS.enc.Hex.parse(
-            '0003333333333333333333333333333333333333333333333333333330',
-          ),
+          CryptoJS.enc.Hex.parse('000102030405060708090a0b0c0d0e0f'),
           {
-            iv: CryptoJS.enc.Hex.parse(
-              '101112131415161718191a1b1c1d1e1f101112131415161718191a1b1c1d1e1f101112131415161718191a1b1c1d1e1f101112131415161718191a1b1c1d1e1f',
-            ),
+            iv: CryptoJS.enc.Hex.parse('101112131415161718191a1b1c1d1e1f'),
             mode: CryptoJS.mode.CFB,
           },
         )
@@ -180,73 +173,84 @@ QkZwYG5l/aKC2vil+Un5Fgydd48cGcOPpQIDAQAB
   </div>
 )
 
-function App() {
-  const private_rsa = `-----BEGIN RSA PRIVATE KEY-----
-  MIIJJwIBAAKCAgBpr7playzJGogXXZFWvcwvpMQWOcv2klGiJkLjE800yRJPDlqS
-  XSQxrxbj64PpOpgkcwhiNdu03k7eg/Yvr1VUoQCvfW0CQ362SelfTcJiwqD63j/9
-  NQswLwkbpqhAiH3mY1NqE8C/wc03zX/XV7Z+IVKiF36gGfdiCIfQTLC1u+g+NRhW
-  P7+HlExLTLuy69r9EvVEdL4pLgZVy+NSwgoHRBVV2R96LkLnJQV4i2MnM+Jx+uUm
-  r5sqZf7j2z6FZOorAETXVj9S5ob9TWxRNv+vG1QbPvMqJP7TMqMx40LcKefEKJhD
-  iqUtgcxUq+eiBngJOWaEPLXtn0I2EjYTetBeQEoBQ3DnPQN4/pjJx/cjxPx+R5ss
-  cemMME59C2Cwp8Or74Asty1a5UUUS36l+BB+jHYqRMZjIf5B228Lo/Q8umnnVcW0
-  Rr+nSZc1UMwhNNs5l0x412tQOgUK09ImurnhPPNvw15h88HbvLnH50EULhbzftsp
-  Azo/5KDasspNrE62KfO9FfpanGSYps+dM333YefiFuXuatvw373F/OeVeIDbMRui
-  fCiMTjMGUTSifLS6eIIL1sq+sCiEVFmZjPb3LIQlLUuT0QfUSnmAVd8X45EHGBW1
-  1fJT80A8QXOcuI+K/zUndCBh9SjAQtet4FhnG67Iwg0Kxf6jEA7+8/XgFQIDAQAB
-  AoICAFJIuDYYMKFOn+CZP/wp/5ykiurHCw8pspctdOw5UY6kcjXM9iV4kSKBsK+5
-  gVWIAkZ2rXNRfYKfhwZl9boAp84hIFiBreqSREiG2UUTVKHx6MYJXga1UsWsCUpX
-  Ai5kVqmWca5oPKzPpgipXbx6lSI0rZJ3+mWKcfoIrD04SGjmKRat3gngzi0wAAqs
-  boMKcNCS/xTSU+/Z3qLUAGRBoO8xSe8yAfjtpbczMsQ8ufEey1J0xg7NsGBdUwty
-  eoX/IPZj2jVR+DGCPF1g91FwIbfi7iPcahRJA4HJrhe8WiTyF3m5JXuIqbv5j+3o
-  fYq/wJtl7nRMlFSypylUGngmKa4HXUvXxNTlZPh4WV41JWKqIrsDQSyEK9DzYXTM
-  bXsfgry6xDKqv8CrIE3cWGMqBLkoAe0JjOUxFliHhv//Stwr038wQLT3r07liT5w
-  wfs4NJzjBgj1ibO51L1EkqlpaQfy5yQFM1TEPKmzL92sMXIn234K/b10fSap1q6o
-  fzqminqm6hyThyvrirXkHmwCEcLiNwzYYoy4CkyfT3jdDhsnqePPdZU3aKtJ6T86
-  KPet8vxONYaQwPhnYPwT9sSzKQal3KVX4cWO0+HRv7ADj7PjdwYJ8MQDzRrRy4TP
-  T5qLN1OqkoSie6IbrArc5wRt4ySVR5xKVbLVeZLqFRJLN+hBAoIBAQCwWBY0wXXk
-  ML2n9JMXfsnPVuEhewvYtmacjsEcGDSXC/qpb2NMnJzi2v+I+CBfY3sZQqOmSMp4
-  kfWwGgt2Lkx2d8sNtls01tPewG8RFe8J4sujc9HbjSVSAa9gBHbsWt42U30JyPGd
-  HIdI5LoU2D4qLn14ZCYqeBlSRDW9LD8hamjwLS3kURoHI8dOnSMIkEhLXSFDymaO
-  hQ4CErX2FXsBWEOHl5i/pZN17+ThVlvNUjGO2JUruhnmGdrs1M+XUkKIvA/NgMzw
-  miuxDZkAhH0y26OBomdH1L3Q3sIa38EpJwk7rT7unRLZW2h6dECHId807X8f8sDc
-  Fr9xhFGa1K3FAoIBAQCZbP9yXNZLvn+RSvDSrdhed93j6cry71HAjNBuKllPwjt3
-  8QA/u12mk1t0B+vt24gW9zHFjzpW6dA/D9OeuuDf1xpkKqKVkrf4JCdtD0FnbEKL
-  xV3PlpObQ3Ifj5WleBiDwIUVXucPuKQmj2/OeHAFJ3EZ23ULv+mJxVMeatxv+/OX
-  pIP6OlnZzzJ+D13X6naR60XXlLMHwflR0JHnJcnSK16a+/CBUyuQszAcL69nR7Y8
-  l1zg8QUTnyxjJ5EkBKyyvHkIPL07D5lPEylZCbXkdLNM4owbkYZOQlFP3JJpO7/f
-  H9EEJPf21wxj7j1s1501BD0V6eCeMKLGPhLMaF4RAoIBAH90q1gmHBd6QxoJ4+av
-  dR2vJbkVPlK8Qx4U7+5mcFxcBChCofrVl28/6kINVRicxnRTfgTJx82tcW5+5Jne
-  jNVzcAfRLcrbGvd9DsnCmKsEiTryk1OkiLzlCuTTVUEvzDxny5Au9LMh04YUC4rV
-  YNkMERPbDtrTQzn6ovy/5kE4r/AoN2s2UyGl8oAXgyLFZftSrKBawhVwhddpvqrG
-  i8lFCkoOgBDEk/hCE6Pqzx9vsPIWCHs6X024kktq5T/x7LT3eFSIK5gbVkj6EwQU
-  vojrxxoDT7LTSP6WgqT55Jrh5AKGDV5ZmS4Qq0sWiy0ghjuzDJcC1fI9XtGlWXYX
-  EiECggEAe9W3RPZb3V0QGBSV1maNCorTZR8SOQ0jPjgqD5FM7wyF9WRnYAl6KHYt
-  zbSLIDweVvMEkEHvw7EK7at3klmiIem7mnezLOj0ZmvACs3Sa4i9GZWfB2uxDhPd
-  LE43agxKNSWSBVLJSy4sCBekFdGuEOrp5gX1Lhujgxjq4tU7wYNp9M7L2493cE9F
-  oOwEQb80Hsj4YW3bJvrQotGmwhwFDwcEYDHTyH553lxTC50NhV4jYDqHs+O1xkWm
-  jQ2ln53KSqXHRvfHgMExa9q6d+xXjOMIyZjdn27RKZ9rg/szjY61aoGCskaogerH
-  TG1B1PUfJWibaXGRY/07lKkLgUMIEQKCAQEAoJ446z4Xs+ZbDL46YWUaUskof5xg
-  UmyIDWspxfKXNBrMYrxzZlmHRRXae5XrPS6OciRLBz7xikAES4mcY/0DjXgMTKIl
-  g5FD3xsxo9dJJSru/kF6ydNkI79xRhM++DbEi86s2ztxLHw7sXuCXSVDHzzdiKqz
-  WP/TvG5EGZGWnzi5EGsYV0PGUK+pnsF/d5MYJGIgzJ6sJ+6F7oknIhPtPxXPXBbW
-  CmjOWx133DkKSV/eKKeXF5xXSEnn/GeESVQU34jQ5rXdteIqFdTr3jzYsZ7VKJ5X
-  b5w8DycrSsZ99ZfwF1PM7ou+fOjKmf0pUmFO4xhch8YfCzPk5rHgYA1mRA==
-  -----END RSA PRIVATE KEY-----`
+const modeToTextMap = {
+  CBC: CryptoJS.mode.CBC,
+  CFB: CryptoJS.mode.CFB,
+  CTR: CryptoJS.mode.CTR,
+  OFB: CryptoJS.mode.OFB,
+  ECB: CryptoJS.mode.ECB,
+}
 
-  const public_rsa = `-----BEGIN PUBLIC KEY-----
-  MIICITANBgkqhkiG9w0BAQEFAAOCAg4AMIICCQKCAgBpr7playzJGogXXZFWvcwv
-  pMQWOcv2klGiJkLjE800yRJPDlqSXSQxrxbj64PpOpgkcwhiNdu03k7eg/Yvr1VU
-  oQCvfW0CQ362SelfTcJiwqD63j/9NQswLwkbpqhAiH3mY1NqE8C/wc03zX/XV7Z+
-  IVKiF36gGfdiCIfQTLC1u+g+NRhWP7+HlExLTLuy69r9EvVEdL4pLgZVy+NSwgoH
-  RBVV2R96LkLnJQV4i2MnM+Jx+uUmr5sqZf7j2z6FZOorAETXVj9S5ob9TWxRNv+v
-  G1QbPvMqJP7TMqMx40LcKefEKJhDiqUtgcxUq+eiBngJOWaEPLXtn0I2EjYTetBe
-  QEoBQ3DnPQN4/pjJx/cjxPx+R5sscemMME59C2Cwp8Or74Asty1a5UUUS36l+BB+
-  jHYqRMZjIf5B228Lo/Q8umnnVcW0Rr+nSZc1UMwhNNs5l0x412tQOgUK09Imurnh
-  PPNvw15h88HbvLnH50EULhbzftspAzo/5KDasspNrE62KfO9FfpanGSYps+dM333
-  YefiFuXuatvw373F/OeVeIDbMRuifCiMTjMGUTSifLS6eIIL1sq+sCiEVFmZjPb3
-  LIQlLUuT0QfUSnmAVd8X45EHGBW11fJT80A8QXOcuI+K/zUndCBh9SjAQtet4Fhn
-  G67Iwg0Kxf6jEA7+8/XgFQIDAQAB
-  -----END PUBLIC KEY-----`
+function App() {
+  const [inputText, setInputText] = useState('')
+  const [outputText, setOutputText] = useState('')
+  const [keyText, setKeyText] = useState('')
+  const [ivText, setIvText] = useState('')
+  const [algorithm, setAlgorithm] = useState('aes')
+  const [mode, setMode] = useState('CBC')
+  const [authMode, setAuthMode] = useState('Passphrase')
+
+  const handleEncrypt = (e) => {
+    console.log('input', inputText)
+    console.log('key', keyText)
+    console.log('iv', ivText)
+    console.log('mode', mode)
+    console.log('authMode', authMode)
+
+    e.preventDefault()
+    if (!inputText) return
+    const iv = ivText ? CryptoJS.enc.Hex.parse(ivText) : undefined
+    console.log('iv', iv?.toString())
+
+    if (authMode === 'Passphrase') {
+      const encrypted = AES.encrypt(inputText, keyText, {
+        mode: modeToTextMap[mode],
+      })
+      console.log('encrypted', encrypted.toString())
+      console.log('iv', encrypted.iv.toString())
+      console.log('key', encrypted.key.toString())
+      setOutputText(encrypted.toString())
+    } else {
+      const encrypted = AES.encrypt(
+        inputText,
+        CryptoJS.enc.Hex.parse(keyText),
+        {
+          iv,
+          mode: modeToTextMap[mode],
+        },
+      )
+      console.log('encrypted', encrypted.toString())
+      console.log('iv', encrypted.iv.toString())
+      console.log('key', encrypted.key.toString())
+      setOutputText(encrypted.toString())
+    }
+  }
+
+  const handleDecrypt = (e) => {
+    e.preventDefault()
+    if (!inputText) return
+    const iv = ivText ? CryptoJS.enc.Hex.parse(ivText) : undefined
+    console.log('iv', iv?.toString())
+
+    if (authMode === 'Passphrase') {
+      const decrypted = AES.decrypt(inputText, keyText, {
+        mode: modeToTextMap[mode],
+      })
+      console.log('decrypted', decrypted.toString(CryptoJS.enc.Utf8))
+      setOutputText(decrypted.toString(CryptoJS.enc.Utf8))
+    } else {
+      const decrypted = AES.decrypt(
+        inputText,
+        CryptoJS.enc.Hex.parse(keyText),
+        {
+          iv,
+          mode: modeToTextMap[mode],
+        },
+      )
+      console.log('decrypted', decrypted.toString(CryptoJS.enc.Utf8))
+      setOutputText(decrypted.toString(CryptoJS.enc.Utf8))
+    }
+  }
 
   return (
     <>
@@ -285,23 +289,39 @@ function App() {
                             Input
                           </Label>
                           <Textarea
+                            value={inputText}
+                            onChange={(e) => setInputText(e.target.value)}
                             id="input"
-                            placeholder="We is going to the market."
+                            placeholder="Text to encrypt or decrypt"
                             className="flex-1 lg:min-h-[200px]"
                           />
                         </div>
                         <div className="flex flex-col space-y-2">
-                          <Label htmlFor="key">Key</Label>
+                          {authMode === 'Passphrase' ? (
+                            <Label htmlFor="passphrase">Passphrase</Label>
+                          ) : (
+                            <Label htmlFor="key">Key</Label>
+                          )}
                           <Textarea
                             id="key"
-                            placeholder="Enter your key here. Or leave it empty to generate a random key."
+                            value={keyText}
+                            onChange={(e) => setKeyText(e.target.value)}
+                            placeholder="Enter your passphrase or key here. Leave it empty to generate a random key."
                           />
                         </div>
-                        <div className="flex flex-col space-y-2">
+
+                        <div
+                          className={clsx('flex flex-col space-y-2', {
+                            'pointer-events-none  opacity-0':
+                              authMode === 'Passphrase',
+                          })}
+                        >
                           <Label htmlFor="iv">IV</Label>
                           <Textarea
                             id="iv"
-                            placeholder="Enter your IV here. Or leave it empty to generate a random IV. If needed."
+                            value={ivText}
+                            onChange={(e) => setIvText(e.target.value)}
+                            placeholder="Enter your IV here. Leave it empty to generate a random IV. If needed."
                           />
                         </div>
                       </div>
@@ -311,8 +331,10 @@ function App() {
                             Output
                           </Label>
                           <Textarea
+                            value={outputText}
+                            onChange={(e) => setOutputText(e.target.value)}
                             id="output"
-                            placeholder="We is going to the market."
+                            placeholder="Output will be displayed here"
                             className="flex-1 lg:min-h-[200px]"
                           />
                         </div>
@@ -320,8 +342,9 @@ function App() {
                     </div>
                   </article>
                 </CardContent>
-                <CardFooter>
-                  <Button>Save changes</Button>
+                <CardFooter className="flex justify-between">
+                  {/*                   <Button>Encrypt </Button>
+                  <Button>Decrypt</Button> */}
                 </CardFooter>
               </Card>
               <aside className=" w-[350px] flex-col sm:flex md:order-2">
@@ -344,7 +367,7 @@ function App() {
                     </div>
                     <div className="flex flex-col space-y-1.5">
                       <Label htmlFor="algorithm">Mode</Label>
-                      <Select value="CBC">
+                      <Select value={mode} onValueChange={(e) => setMode(e)}>
                         <SelectTrigger id="mode">
                           <SelectValue placeholder="Select Mode" />
                         </SelectTrigger>
@@ -359,7 +382,11 @@ function App() {
                     </div>
                     <div className="flex flex-col space-y-1.5">
                       <Label htmlFor="auth">Encrypt with</Label>
-                      <RadioGroup id="auth" defaultValue="Passphrase">
+                      <RadioGroup
+                        id="auth"
+                        defaultValue="Passphrase"
+                        onValueChange={(e) => setAuthMode(e)}
+                      >
                         <div className="flex items-center space-x-2">
                           <RadioGroupItem value="Passphrase" id="option-one" />
                           <Label htmlFor="option-one">Passphrase</Label>
@@ -372,8 +399,8 @@ function App() {
                     </div>
                   </CardContent>
                   <CardFooter className="flex justify-between">
-                    <Button>Cancel</Button>
-                    <Button>Deploy</Button>
+                    <Button onClick={handleEncrypt}>Encrypt</Button>
+                    <Button onClick={handleDecrypt}>Decrypt</Button>
                   </CardFooter>
                 </Card>
               </aside>
